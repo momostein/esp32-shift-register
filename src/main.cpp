bd47ee0aed6ccca75a3ac8593732e28e4c2b8a17
Brecht Ooms <brecht.ooms@hotmail.com>
2024-06-11 21:50:10 +0200

    Send UDP packets

--- src/main.cpp
+++ src/main.cpp
@@ -3,6 +3,7 @@
 #include <WiFiUdp.h>
 
 #include "net_config.h"
+#include "panel.h"
 
 const int dataPin = 27;
 const int clockPin = 26;
@@ -61,27 +62,60 @@ void loop()
 {
 	// Load parallel inputs into shift register
 	directWriteLow(loadPin);
+	delayMicroseconds(1);
 	directWriteHigh(clockPin);
+	delayMicroseconds(1);
 	directWriteLow(clockPin);
+	delayMicroseconds(1);
 
 	// Shift inputs and read bit per bit into value
 	directWriteHigh(loadPin);
-	unsigned short value = 0;
 
-	for (size_t bit_i = 0; bit_i < 16; bit_i++)
+	Panel panels[1];
+
+	for (size_t panelIndex = 0; panelIndex < sizeof(panels) / sizeof(Panel); panelIndex++)
 	{
-		unsigned short read = (GPIO.in >> dataPin) & 1;
-		value |= (read << bit_i);
+		for (size_t regIndex = 0; regIndex < 3; regIndex++)
+		{
+			uint8_t reg = 0;
 
-		directWriteHigh(clockPin);
-		directWriteLow(clockPin);
+			for (size_t bit_i = 0; bit_i < 8; bit_i++)
+			{
+
+				uint8_t read = (GPIO.in >> dataPin) & 1;
+				Serial.print(read, BIN);
+
+				reg |= (read << bit_i);
+
+				delayMicroseconds(1);
+				directWriteHigh(clockPin);
+				delayMicroseconds(1);
+				directWriteLow(clockPin);
+				delayMicroseconds(1);
+			}
+
+			panels[panelIndex][regIndex] = reg;
+		}
 	}
 
-	Serial.println(value, BIN);
+	Serial.print(" - ");
+
+	// Serial.println(value, BIN);
+
+	for (size_t panelIndex = 0; panelIndex < sizeof(panels) / sizeof(Panel); panelIndex++)
+	{
+		for (size_t regIndex = 0; regIndex < 3; regIndex++)
+		{
+			uint8_t reg = panels[panelIndex][regIndex];
+			Serial.printf("%02X", reg);
+		}
+		Serial.print(" ");
+	}
+	Serial.println();
 
 	udp.beginPacket(UDP_ADDRESS, UDP_PORT);
-	udp.write((uint8_t *)(&value), sizeof(value));
+	udp.write((uint8_t *)(&panels), sizeof(panels));
 	udp.endPacket();
 
-	delay(500);
+	delay(50);
 }
